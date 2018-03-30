7b3b08522c1cf6ddc9729ceaa754c4ae7db41748
Bushrut <srashaev@rambler.ru>
2018-03-30 21:48:10 +0300

    Оформляет декоративные элементы

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,26 @@
 const feedbackButton = document.querySelector('.feedback-button');
-const modalFeedback = document.querySelector(".modal-feedback");
-const feedbackClose = modalFeedback.querySelector(".modal-button-close");
+const modalFeedback = document.querySelector('.modal-feedback');
+const modalButtonClose = modalFeedback.querySelector('.modal-button-close');
+const modalMap = document.querySelector('.modal-map');
+const mapLink = document.querySelector('.map-link');
+const mapClose = modalMap.querySelector('.modal-button-close');
 
 feedbackButton.addEventListener('click', function(evt) {
   evt.preventDefault();
   modalFeedback.classList.add('modal-show');
 });
 
-feedbackClose.addEventListener('click', function(evt) {
+mapLink.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modalMap.classList.add('modal-show');
+});
+
+modalButtonClose.addEventListener('click', function(evt) {
   evt.preventDefault();
   modalFeedback.classList.remove('modal-show');
 });
 
+mapClose.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modalMap.classList.remove('modal-show');
+});
